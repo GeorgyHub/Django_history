60b64777d57c6a0662317700e62ab24bd561801f
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-28 18:54:53 +0300

    added model

--- mind_maps/app/models.py
+++ mind_maps/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255, null=True)
+    birthday = models.DateTimeField(null=True)
+    deathday = models.DateTimeField()
+    placebirthday = models.CharField(max_length=255, null=True)
+    ptoho = models.ImageField()
+    status = models.BooleanField(null=True)
