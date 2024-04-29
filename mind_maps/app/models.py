5ab883f8eebf9ba553f996737208311e564c0e81
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-29 15:35:18 +0300

    add questions

--- mind_maps/app/models.py
+++ mind_maps/app/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Person(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255, null=True)
-    birthday = models.DateTimeField(null=True)
-    deathday = models.DateTimeField()
+    birthday = models.DateTimeField(auto_now_add=True)
+    deathday = models.DateTimeField(auto_now=False)
     placebirthday = models.CharField(max_length=255, null=True)
-    ptoho = models.ImageField()
+    ptoho = models.ImageField(upload_to='media/%Y/%M/%D')
     status = models.BooleanField(null=True)
+    is_published = models.BooleanField(default=True)
+
+class Questions(models.Model):
+    question = models.TextField(null=True)
