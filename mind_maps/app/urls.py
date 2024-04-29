e3c55712eb21800f5f0e793b9122e5f62838bf04
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-29 13:38:44 +0300

    add reg

--- mind_maps/app/urls.py
+++ mind_maps/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('sing_in/', views.sing_in),
+    path('sing_up/', views.sing_up),
 ]
--- mind_maps/app/views.py
+++ mind_maps/app/views.py
@@ -5,3 +5,9 @@ from django.template import loader
 # Create your views here.
 def home(request):
     return render(request, "base.html")
+
+def sing_in(request):
+    return render(request, "sing_in.html")
+
+def sing_up(request):
+    return render(request, "sing_up.html")
