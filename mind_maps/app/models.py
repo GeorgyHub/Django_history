2dd2d96f153514697270b424233692836186fd8c
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-30 12:03:00 +0300

    edit templates

--- mind_maps/app/models.py
+++ mind_maps/app/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 class Person(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255, null=True)
-    birthday = models.DateTimeField(auto_now_add=True)
-    deathday = models.DateTimeField(auto_now=False)
+    birthday = models.DateField(auto_now_add=True)
+    deathday = models.DateField(auto_now=False)
     placebirthday = models.CharField(max_length=255, null=True)
-    ptoho = models.ImageField(upload_to='media/%Y/%M/%D')
+    ptoho = models.ImageField(upload_to='photo')
     status = models.BooleanField(null=True)
     is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
 
+    def get_full_years(self):
+        pass
+
 class Questions(models.Model):
     question = models.TextField(null=True)
 
     def __str__(self):
         return f"{self.question}"
+
+class Post(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
--- mind_maps/app/urls.py
+++ mind_maps/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('sing_in/', views.sing_in, name='sing_in'),
     path('sing_up/', views.sing_up, name='sing_up'),
 ]
