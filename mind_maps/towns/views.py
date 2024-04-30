a1c694225053c8e10671c58b02fe9b8859e1eb1d
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-30 13:28:34 +0300

    edit templates

--- mind_maps/towns/views.py
+++ mind_maps/towns/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 def town(request):
+    town = Place.objects.all()
     return render(request, 'index.html')
