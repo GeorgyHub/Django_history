94e2572a59c29f775693a612a1327dce9f52e000
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-05-02 00:09:04 +0300

    edit urls

--- mind_maps/app/urls.py
+++ mind_maps/app/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('sing_in/', views.sing_in, name='sing_in'),
-    path('sing_up/', views.sing_up, name='sing_up'),
 ]
